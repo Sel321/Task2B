a8cbc6c5d1262581cd408eac130037f28193d2a1
Sel321 <salsadip@live.com.au>
2016-04-09 09:03:20 +1000

    Update task2b.c

--- task2b.c
+++ task2b.c
@@ -1,4 +1,12 @@
 //task 2B by Salina, Emma and Michael
+/* in this task 
+- understand how the Mandelbrot set is calculated
+have completed all the relevent module 4 and 5 activities.  These are designed to help you.
+carefully plan what you are going to do before getting started, and revise this plan regularly over the course of the task.
+work out with your partner how you will work together, how you will allocate the tasks between yourselves, and how you will help each other.
+record everything you do and everything you find, just like a scientist does in their journal.
+- go to the wiki to record what you do and what you find and what the next steps should be
+
 #include <stdlib.h>
 #include <stdio.h>
 
